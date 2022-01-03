c774bb2eaef96f80dc8e061236e360f79906095f
QXZ1JJS <ThanhDung.Ho@salt-solutions.de>
2022-01-03 21:36:59 +0100

    connection to API

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
+const API_KEY = "63e537c3-a81a-4d9b-8ad6-de09f385231f";
 
 app.set('port', process.env.PORT || 3000);
 
 // req request, res response
 app.get('/api/coins', function (req, res) {
-    axios.get('https://api.coinmarketcap.com/v2/ticker/?limit=100').then(function (response) {
+    axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=100', {
+        headers: {
+            'X-CMC_PRO_API_KEY': API_KEY
+        }
+    }).then(function (response) {
         // client should not cache response of server
         // json response dynamic
         res.setHeader('Cache-Control', 'no-cache');
         // set response successfully
         // data as part of response 
         // res = from express that we finally set; response = response sending from an axios call
+        // send data on sucess
         res.json(response.data);
     }).catch(function (error) {
         console.log('api call failed:(', error);
@@ -21,3 +27,22 @@ app.get('/api/coins', function (req, res) {
 })
 
 // api call for globalMarketData
+
+app.get('/api/market_data', function (req, res) {
+    axios.get('https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest ', {
+        headers: {
+            'X-CMC_PRO_API_KEY': API_KEY
+        }
+    }).then(function (response) {
+        res.setHeader('Cache-Control', 'no-cache');
+        res.json(response.data);
+    }).catch(function (error) {
+        console.log('api call failed:(', error);
+    })
+})
+
+// final step --> server bind and listen to specific port
+// set the port earlier
+app.listen(app.get('port'), function () {
+    console.log('Find the server at http://localhost:' + app.get('port'));
+});
